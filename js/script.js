fbc3bc65932287c6e5673a6e01c52e57b0206647
Billy Tseng <tseng.billy@gmail.com>
2017-09-28 17:40:12 -0700

    feat: Show street view in the background.

--- js/script.js
+++ js/script.js
@@ -14,7 +14,21 @@ function loadData() {
     // load streetview
 
     // YOUR CODE GOES HERE!
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ", " + cityStr;
+    $greeting.text('So, you want to live at ' + address + '?');
+    var apiKey = "YOUR_API_KEY";
+    var imgStr = "";
+    imgStr += "https://maps.googleapis.com/maps/api/streetview?";
+    imgStr += "size=600x400&";
+    imgStr += "location=" + address + "&";
+    imgStr += "key=" + apiKey;
 
+    var htmlStr = "";
+    htmlStr += '<img class="bgimg" ';
+    htmlStr += 'src="' + imgStr + '">';
+    $body.append(htmlStr);
     return false;
 };
 
